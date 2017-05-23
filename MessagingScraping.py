8e54ecc8d5bb8ad16e15c4dda4c259d319609171
Justin-Kim72 <justinkim1@Berkeley.edu>
2017-05-22 18:14:22 -0700

    Add files via upload

--- MessagingScraping.py
+++ MessagingScraping.py
@@ -3,6 +3,8 @@ from tweepy import OAuthHandler
 import os 
 from twilio.rest import Client
 
+
+
 account_sid = os.environ["TWILIO_ACCOUNT_SID"]
 auth_token = os.environ["TWILIO_AUTH_TOKEN"]
 
@@ -13,6 +15,9 @@ client.messages.create(
     from_ = "+14352141522"
     body = "")
 
+
+#This the code for Twilio, somehow the body would where the twitter post comes from that
+#relates to the message "completion of going for a run."
  
 consumer_key = 'YOUR-CONSUMER-KEY'
 consumer_secret = 'YOUR-CONSUMER-SECRET'
@@ -29,6 +34,8 @@ api = tweepy.API(auth)
 for tweet in tweepy.Cursor(api.user_timeline).items():
     process_or_store(tweet._json)
 
+#This is the twitter authentification that would go through a person's profile and list all the tweets
+
 fb_url = https://{MessagingScraping}.firebaseio.com/tweets.json
 
 def check_tweet(tweet):
@@ -40,6 +47,7 @@ def check_tweet(tweet):
                 return False
     finally:
         return True
+#This is checking if the tweet exists in the database
 def search(tag):
     try:
         c = tweepy.Cursor(api.search, q=tag)
@@ -54,7 +62,39 @@ def search(tag):
                     i += 1
                     break
 
+
 while True:
-    tags = ["#danceparty", "#ilovehats", "#firebase", "#tweepy"]
+    tags = ["#completion of run", "#run", "#finishedrun", "#Running"]
     search(random.choice(tags))
     time.sleep(60)
+
+#This is checking through all the tags that have anything to do with running
+
+final FirebaseDatabase database = FirebaseDatabase.getInstance();
+DatabaseReference ref = database.getReference("server/saving-data/fireblog");
+
+public static class Posts {
+
+    public String date_of_post;
+    public String username;
+    public String post_content;
+
+
+
+    public Posts(String date_of_post, String username, String post_content) {
+        // ...
+    }
+
+}
+
+DatabaseReference postsRef = ref.child("posts");
+
+Map<String, Posts> posts = new HashMap<String, Post>();
+usersRef.child("@djkhaled").setValue(new User("April 28 2017", "Available everywhere! #DJKHALED   #IMTHEONE FEAT. @justinbieber, @QuavoStuntin, @chancetherapper & @LilTunechi"));
+usersRef.child("@drake").setValue(new User("April 25 2017", "Hosting first-ever NBA Awards on TNT, June 26th - tune in."));
+
+usersRef.setValue(posts);
+
+#This would kind of be how the firebase would work, depending on how you would do it, but let's say if we did
+#it with multiple people and looking for a post that fits our description, we would add the date, the username, and the
+#post content to the firebase database. 
